43789f45cef2e88ffb63dddf031b9eed7dfa234c
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-10-27 19:47:07 +0530

    Main File

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,StatusBar,SafeAreaView } from 'react-native';
+import Login from './App/Screens/Login';
+import { ClerkProvider,SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './App/Navigations/TabNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey={"pk_test_Y29vbC1jaGVldGFoLTU4LmNsZXJrLmFjY291bnRzLmRldiQ"}>
+      <SafeAreaView style={styles.container}> 
+        <SignedIn>
+            <NavigationContainer>
+              <TabNavigation/>
+            </NavigationContainer>
+        </SignedIn>
+        <SignedOut>
+          <Login/>
+        </SignedOut>
+      </SafeAreaView>
+      <StatusBar/>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +26,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
