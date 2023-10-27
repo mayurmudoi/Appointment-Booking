df895df54e483c755586f0f2afcb5d04523ea49e
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-10-27 20:28:43 +0530

    searchbar update in home.js

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -2,12 +2,14 @@ import { View, Text,Button } from 'react-native'
 import React from 'react'
 import {useAuth} from "@clerk/clerk-expo";
 import Header from '../Components/Home/Header';
+import SearchBar from '../Components/Home/SearchBar';
 
 export default function Home() {
 const { isLoaded,signOut } = useAuth();
   return (
     <View style={{padding:20, marginTop:20}}>
       <Header/>
+      <SearchBar setSearchText={(value)=>console.log(value)}/>
         {/* <Button
             title="Sign Out"
             onPress={() => {
