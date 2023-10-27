af3fe44ad8794590509cbbdd4953a86b9f26a1b1
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-10-28 00:53:02 +0530

    Fonts imported

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import Login from './App/Screens/Login';
 import { ClerkProvider,SignedIn, SignedOut } from '@clerk/clerk-expo';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigation from './App/Navigations/TabNavigation';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'appfont': require('./assets/fonts/Outfit-Regular.ttf'),
+    'appfont-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'appfont-semi': require('./assets/fonts/Outfit-SemiBold.ttf'),
+  });
+  if(!fontsLoaded){
+    return null;
+  }
   return (
     <ClerkProvider publishableKey={"pk_test_Y29vbC1jaGVldGFoLTU4LmNsZXJrLmFjY291bnRzLmRldiQ"}>
       <SafeAreaView style={styles.container}> 
