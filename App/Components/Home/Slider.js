26f8dd151eba9d8a10df94534d42c58d5116d7a4
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-11-01 20:03:15 +0530

    using API to get Image

--- App/Components/Home/Slider.js
+++ App/Components/Home/Slider.js
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, Dimensions} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../../Services/GlobalApi'
 
 export default function Slider() {
-  return (
-    <View>
-      <Text>Slider</Text>
+  const [sliderList, setSliderList]=useState();
+  // const sliderList=[
+  //   {
+  //             id:1,
+  //             name:'Slider 1',
+  //             imageUrl:'https://mobisoftinfotech.com/resources/wp-content/uploads/2018/07/Banner-1.png'
+  //         },
+  //         {
+  //             id:2,
+  //             name:'Slider 2',
+  //             imageUrl:'https://img.freepik.com/premium-vector/medical-healthcare-online-consultation-banner_42775-520.jpg'
+  //           }
+  //         ]
+
+          useEffect(()=>{
+            getSlider();
+          },[])
+          const getSlider=()=>{
+            GlobalApi.getSlider().then(resp=>{
+              // console.log(resp.data.data)
+              setSliderList(resp.data.data)
+            })
+          }
+  return ( 
+    <View style={{margin:10}}>
+      <FlatList data={sliderList}
+        horizontal={true}
+        renderItem={({item})=>(
+          <Image source={{uri:item.attributes.Image.data.attributes.url}}
+          style={{width: Dimensions.get('screen').width*0.8, height:170, borderRadius:10, margin:2}}/>
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   )
 }
